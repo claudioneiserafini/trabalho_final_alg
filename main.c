9ef8d2bc8cc4ada7dd9888eb4443f3e9a67f3800
Claudionei Lovato Serafini <80057508+claudioneiserafini@users.noreply.github.com>
2021-07-12 10:15:12 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Criando struct:
+typedef struct sPi{
+	float x;
+	float y;
+}Pi;
+
+//Prototipação:
+
+
+//Implementação:
+int main(void){
+  printf("Hello World!");
+  return 0;
+}
