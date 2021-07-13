f0974dfef645361bfa08c710d04bd30a65733e5e
Claudionei Lovato Serafini <80057508+claudioneiserafini@users.noreply.github.com>
2021-07-13 16:02:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,66 @@
+// |------------------------------------------------ Bibliotecas --------------------------------------------------|
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-//Criando struct:
+// |-------------------------------------------------- Struct -----------------------------------------------------|
+
 typedef struct sPi{
 	float x;
 	float y;
 }Pi;
 
-//Prototipação:
+// |------------------------------------------------ Prototipação -------------------------------------------------|
 
+void gerarPontos();
+int calculaPontosQuadrado();
+int calculaPontosCirculo();
+float calculaDistancia();
+float calculaValorPi();
+void mostraValorPi();
 
-//Implementação:
+// |----------------------------------------------- Implementação -------------------------------------------------|
+
+//
 int main(void){
   printf("Hello World!");
   return 0;
 }
+
+//
+void gerarPontos(int tam){
+  int i;
+  srand(time(NULL));
+  for(i=0; i < tam; i++){
+	  
+  }
+
+}
+
+//
+int calculaPontosQuadrado(){
+  return 0;
+}
+
+//
+int calculaPontosCirculo(){
+  return 0;
+}
+
+//
+float calculaDistancia(){
+  return 0;
+}
+
+//
+float calculaValorPi(){
+  return 0;
+}
+
+//
+void mostraValorPi(){
+  
+}
+
+// |---------------------------------------------- FIM DO PROGRAMA ------------------------------------------------|
